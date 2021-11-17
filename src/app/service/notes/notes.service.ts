dd5b4d9bb8dde148d7595fbf33b6938b51cf666e
KevinGuzmanH <82849364+KevinGuzmanH@users.noreply.github.com>
2021-11-17 17:07:32 -0500

    1

--- src/app/service/notes/notes.service.ts
+++ src/app/service/notes/notes.service.ts
@@ -10,7 +10,7 @@ import {UpdateNote} from "../../model/UpdateNote";
 })
 export class NotesService {
 
-  URL = 'https://notesbackapi.herokuapp.com/Notes/V1';
+  URL = 'https://notesbackapi.herokuapp.com/security/V1/Notes/V1';
   Client: HttpClient;
 
   constructor(HttpClient: HttpClient) {
